e3327c110f63f4eaf8ddebed679192c3db6110e9
TerminalNabendu <deeprauth355@gmail.com>
2026-03-23 16:10:00 +0530

    Added basic if statement program

---
+++ C Journey/Chapter 3/01_Basic_if_statement.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+
+    // Check if age is greater than 18
+    if (age > 18) {
+        printf("You can drive\n"); // Executes if condition is true
+    }
+
+    return 0;
+}

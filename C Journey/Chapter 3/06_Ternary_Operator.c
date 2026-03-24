9d52c1a48c1d1a00baa5efc7de4a75d469f08679
TerminalNabendu <deeprauth355@gmail.com>
2026-03-24 10:36:40 +0530

    added short if else

---
+++ C Journey/Chapter 3/06_Ternary_Operator.c
@@ -0,0 +1,12 @@
+// Short if-else 
+
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+
+    // condition ? true : false
+    (age > 18) ? printf("Adult\n") : printf("Minor\n");
+
+    return 0;
+}

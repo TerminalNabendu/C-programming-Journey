ce9e4fd412d9025184d76879388d97a865ec45fb
TerminalNabendu <deeprauth355@gmail.com>
2026-03-24 19:58:38 +0530

    added switch cases

---
+++ C Journey/Chapter 3/07_Switch_Case.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int choice = 2;
+
+    switch (choice) {
+        case 1:
+            printf("You chose option 1\n");
+            break; // stops execution here
+
+        case 2:
+            printf("You chose option 2\n");
+            break;
+
+        case 3:
+            printf("You chose option 3\n");
+            break;
+
+        default:
+            printf("Invalid choice\n");
+    }
+
+    return 0;
+}

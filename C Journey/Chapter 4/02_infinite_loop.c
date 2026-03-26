a0c09a52ee1dcc735ab0a609ad95d46244968c62
TerminalNabendu <deeprauth355@gmail.com>
2026-03-26 11:12:01 +0530

    added infinite loop

---
+++ C Journey/Chapter 4/02_infinite_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int i =0;
+while(i<10){
+    printf("The value of i: %d\n", i);
+}
+
+//or
+
+while(2<10){
+    printf("The value of i: %d\n", i);
+}
+    return 0;
+}

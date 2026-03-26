3043382d52194a852e4d88e3fe29eb7b2514e28a
TerminalNabendu <deeprauth355@gmail.com>
2026-03-26 10:52:21 +0530

    Update 01_while_loop.c

--- C Journey/Chapter 4/01_while_loop.c
+++ C Journey/Chapter 4/01_while_loop.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    int i = 0;  // loop counter initialization
+int main()
+{
+    int i = 0; // loop counter initialization
 
-    while (i < 10) {  // condition check (runs while true)
+    while (i < 10)
+    { // condition check (runs while true)
         printf("Value of i = %d\n", i);
 
-        i++;  // IMPORTANT: increment to avoid infinite loop
+        i++; // IMPORTANT: increment to avoid infinite loop
         //(i = i + 1)
     }
-
+    // The moment i reaches 10 loop terminates
     return 0;
 }
